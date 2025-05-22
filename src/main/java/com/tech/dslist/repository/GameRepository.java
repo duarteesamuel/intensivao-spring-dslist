1b43c92e25ecde6750e97b625a0f5ec0789502aa
Samuel Duarte <sduartegit@gmail.com>
2025-05-22 09:50:55 -0300

    Homolog OK

--- src/main/java/com/tech/dslist/repository/GameRepository.java
+++ src/main/java/com/tech/dslist/repository/GameRepository.java
@@ -12,12 +12,16 @@ import java.util.List;
 public interface GameRepository extends JpaRepository<Game, Long> {
 
     @Query(nativeQuery = true, value = """
-            SELECT tb_games.id, tb_games.title, tb_games.game_year AS `year`, tb_games.img_url AS imgUrl,
-            tb_games.short_description AS shortDescription, tb_belonging.position
-            FROM tb_games
-            INNER JOIN tb_belonging ON tb_games.id = tb_belonging.game_id
-            WHERE tb_belonging.list_id = :listId
-            ORDER BY position
-            """)
+    SELECT tb_games.id, 
+           tb_games.title, 
+           tb_games.game_year AS year, 
+           tb_games.img_url AS img_url,
+           tb_games.short_description AS short_description, 
+           tb_belonging.position
+    FROM tb_games
+    INNER JOIN tb_belonging ON tb_games.id = tb_belonging.game_id
+    WHERE tb_belonging.list_id = :listId
+    ORDER BY tb_belonging.position
+    """)
     List<GameMinProjection> searchByList(Long listId);
 }
